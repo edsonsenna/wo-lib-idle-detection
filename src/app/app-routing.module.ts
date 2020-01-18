86458fd7e1b1ce059e644fcb006c9b02510c0805
Edson de Senna Junior <edsondesennajunior@MacBook-Air-de-Edson.local>
2020-01-18 19:36:39 -0300

    Modifying routes rule for any unconfigured route redirects to home route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
   {
-    path: '',
+    path: '**',
     redirectTo: 'home',
     pathMatch: 'full'
   },
